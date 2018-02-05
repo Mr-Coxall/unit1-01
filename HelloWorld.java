a621d48bbbed112f6bb578bb71156411ab9073b3
Mr. Coxall <mr.coxall@mths.ca>
2018-02-05 12:04:33 -0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Created by: Mr. Coxall
+ *  Created on: Jan 2018
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
